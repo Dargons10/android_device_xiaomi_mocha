cd0b6365e5cc96e27081ab2be33772199c3568c8
Vitaliy Tomin <highwaystar.ru@gmail.com>
2017-01-15 13:20:37 +0800

    Fix build.prop tags
    * remove TARGET_UNIFIED_DEVICE because we only build it for one device
    * remove custom init code because no need to set anything at runtime
    * updated PRODUCT_MODEL to MI PAD as it set in stock rom
    
    Change-Id: I372a257930efb6faac4f68ff1486eeb1aac07346

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -145,9 +145,3 @@ BOARD_SEPOLICY_UNION += \
 	wifi_loader.te \
 	wpa.te \
 	zygote.te
-
-# Vendor Init
-TARGET_UNIFIED_DEVICE := true
-#TARGET_INIT_VENDOR_LIB := libinit_tn8
-#TARGET_LIBINIT_DEFINES_FILE := device/xiaomi/mocha/init/init_tn8.c
-
