97156a905d2488dcda0d0c2aabb5cab7a2ef38e8
Vitaliy Tomin <highwaystar.ru@gmail.com>
2017-02-16 20:30:12 +0800

    mocha: move to mini_tablet_wifionly template due to small /system partition size

--- lineage.mk
+++ lineage.mk
@@ -2,7 +2,7 @@
 $(call inherit-product, device/xiaomi/mocha/full_mocha.mk)
 
 # Inherit some common CM stuff.
-$(call inherit-product, vendor/cm/config/common_full_tablet_wifionly.mk)
+$(call inherit-product, vendor/cm/config/common_mini_tablet_wifionly.mk)
 
 PRODUCT_NAME := lineage_mocha
 PRODUCT_DEVICE := mocha
