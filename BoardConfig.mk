a8d39a2a65bcd983505782a7f4faf43beb4ee7d7
Vitaliy Tomin <highwaystar.ru@gmail.com>
2017-03-08 23:08:02 +0800

    mocha: partial offline charging fix
    * shows charging animation once, but doesnt switches up screen
    on power button to show battery level

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -76,6 +76,9 @@ BOARD_HARDWARE_CLASS := \
     hardware/cyanogen/cmhw \
     device/xiaomi/mocha/cmhw
 
+# Charger
+BOARD_CHARGER_DISABLE_INIT_BLANK := true
+
 # Include an expanded selection of fonts
 EXTENDED_FONT_FOOTPRINT := true
 
