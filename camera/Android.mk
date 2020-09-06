53a1508799ac55dcdce334f9ed0ca3247888b80e
Dargons10 <jdamr2p@gmail.com>
2020-09-06 22:45:31 +0200

    mocha: Update android.mk for camera wrapper

--- camera/Android.mk
+++ camera/Android.mk
@@ -1,4 +1,5 @@
 LOCAL_PATH := $(call my-dir)
+
 include $(CLEAR_VARS)
 
 LOCAL_SRC_FILES := \
@@ -6,11 +7,14 @@ LOCAL_SRC_FILES := \
     CameraWrapper.cpp
 
 LOCAL_SHARED_LIBRARIES := \
-    libhardware liblog libcamera_client libutils libcutils libcamera_metadata
+    libhardware liblog  libcamera_client libutils libgui libhidltransport libcamera_metadata libsensor android.hidl.token@1.0-utils android.hardware.graphics.bufferqueue@1.0
 
 LOCAL_C_INCLUDES += \
+    framework/native/include \
     system/core/include \
     system/media/camera/include
+
+LOCAL_HEADER_LIBRARIES := libnativebase_headers
 
 LOCAL_32_BIT_ONLY := true
 #LOCAL_MODULE_PATH := $(TARGET_OUT_SHARED_LIBRARIES)/hw
