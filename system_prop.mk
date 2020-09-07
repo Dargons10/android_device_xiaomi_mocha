eccc7707a878cadb34570a278c1be4c54a7cd0df
Dargons10 <jdamr2p@gmail.com>
2020-09-07 19:15:39 +0200

    mocha: Added system_prop and reworked system.prop

---
+++ system_prop.mk
@@ -0,0 +1,50 @@
+# AptX
+PRODUCT_PROPERTY_OVERRIDES += \
+    persist.bt.enableAptXHD=true \
+    persist.service.btui.use_aptx=1 \
+    persist.vendor.bt.a2dp_offload_cap=sbc-aptx-aptxtws-aptxhd-aac-ldac \
+    persist.vendor.btstack.a2dp_offload_cap=sbc-aptx-aptxtws-aptxhd-aac-ldacs
+
+# BT
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.bt.bdaddr_path=/data/mocha_btmacaddr.txt
+
+# DRM
+PRODUCT_PROPERTY_OVERRIDES += \
+    drm.service.enabled=true
+
+# Graphics
+PRODUCT_PROPERTY_OVERRIDES += \
+    persist.sys.ui.hw=true \
+    debug.sf.disable_backpressure=1 \
+    debug.sf.latch_unsignaled=1 \
+    debug.hwui.render_dirty_regions=false
+
+# Lineage genuine
+PRODUCT_PROPERTY_OVERRIDES += \
+    persist.lineage.nofool=true
+
+# OMX
+PRODUCT_PROPERTY_OVERRIDES += \
+    persist.media.treble_omx=false
+
+# Radio
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.radio.noril=yes
+
+# Storage
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.sys.sdcardfs=true
+
+# Wifi
+PRODUCT_PROPERTY_OVERRIDES += \
+    persist.debug.wfd.enable=1
+
+
+PRODUCT_DEFAULT_PROPERTY_OVERRIDES += \
+	persist.sys.usb.config=mtp
+
+PRODUCT_PROPERTY_OVERRIDES += \
+  persist.service.adb.enable=1 \
+  persist.service.debuggable=1 \
+  persist.sys.usb.config=mtp,adb
